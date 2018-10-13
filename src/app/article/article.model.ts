8e4a5a76b354b1c914136b37e91f73896dccee13
Edmund Wright <nekurasky@gmail.com>
2018-10-12 21:28:38 -0500

    Can now handle urls withou http:// prefix

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -20,8 +20,10 @@ export class Article {
   domain(): string {
     try {
       // e.g. http://foo.com/path/to/bar
-      const domainAndPath: string = this.link.split('//')[1];
+      const splitLink: string[] = this.link.split('//');
       // e.g. foo.com/path/to/bar
+      const domainAndPath: string = splitLink.length > 1 ? splitLink[1] : splitLink[0];
+      // e.g. foo.com
       return domainAndPath.split('/')[0];
     } catch (err) {
       return null;
